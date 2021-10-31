42a3bf29a1d9d7789a868c54efccd6506002737f
Priyanka Hiranandani <priyankahiranandani@gmail.com>
2021-10-31 07:23:41 +0530

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -6,7 +6,7 @@ def bubbleSort(arr):
     # Traverse through all array elements 
     for i in range(n-1): 
     # range(n) also work but outer loop will repeat one time more than needed. 
-  
+        flag = 0
         # Last i elements are already in place 
         for j in range(0, n-i-1): 
   
@@ -14,7 +14,11 @@ def bubbleSort(arr):
             # Swap if the element found is greater 
             # than the next element 
             if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag = 1
+        
+        if flag == 0: #flag is 0 after the loop travsersal implies array is already sorted.
+          break;
   
 # Driver code to test above 
 arr = [64, 34, 25, 12, 22, 11, 90] 
